b0a70d74dfb7d8c983f83b15ef4624a891f64036
Mami14331 <mami.fbe@gmail.com>
2025-07-15 14:27:48 +0300

    Update 0-javascript_is_amazing.js

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,1 +1,7 @@
+git init
+git add 0-javascript_is_amazing.js
+git commit -m "Add 0-javascript_is_amazing.js"
+git branch -M main
+git remote add origin https://github.com/Mami14331/JS-Fundamentals.git
+git push -u origin main
 
